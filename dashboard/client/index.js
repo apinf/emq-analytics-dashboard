2e113d2872a80e7d40b1bb84075f36e9dbca9641
Damir Mustafin <frenchthebread@gmail.com>
2017-01-20 06:57:12 +0200

    update

--- dashboard/client/index.js
+++ dashboard/client/index.js
@@ -25,32 +25,46 @@ Template.dashboard.onCreated(function(){
 
   instance.parseDataForNV = data => {
 
-    let arr = [];
-    let ticks = [];
 
-    const a = _.map(data, item => {
-      item.day = moment(item.date).format('DD-MM-YYYY');
-      return item;
-    })
-
-
-
-    const b = _.groupBy(a, 'day');
-
-    for (key in b) {
-      arr.push([moment(key, 'DD-MM-YYYY').valueOf(), b[key].length])
-      ticks.push(moment(key, 'DD-MM-YYYY').valueOf())
-    }
+    const dataWithNormalizedDate = instance.normalizeDate(data)
+    const grouppedByDate = instance.groupBy(dataWithNormalizedDate, 'day')
+    const dataForNV = instance.buildDataForNV(grouppedByDate);
+    const sortedByDate = instance.sortByDate(dataForNV);
 
     return {
       data: [
         {
-          key: 'Series 1',
-          values: arr
+          key: 'MQT connections over time',
+          values: dataForNV
         }
-      ],
-      ticks
+      ]
     }
+  }
+
+  instance.normalizeDate = (data) => {
+    return _.map(data, item => {
+      item.day = moment(item.date).format('DD-MM-YYYY');
+      return item;
+    })
+  }
+
+  instance.groupBy = (data, key) => {
+    if (key === 'day') {
+      return _.groupBy(data, 'day');
+    }
+  }
+
+  instance.buildDataForNV = (data) => {
+    let dataForNV = [];
+    for (key in data) {
+      dataForNV.push({ x: moment(key, 'DD-MM-YYYY').valueOf(), y: data[key].length })
+    }
+    return dataForNV;
+  }
+
+  instance.sortByDate = (data) => {
+    console.log(_.sortBy(data, (dataItem) => dataItem.x));
+    return _.sortBy(data, (dataItem) => dataItem.x)
   }
 
   instance.render = (chartData) => {
@@ -61,37 +75,56 @@ Template.dashboard.onCreated(function(){
 
     if (dataForNV.data) {
       nv.addGraph(function() {
-        var chart = nv.models.cumulativeLineChart()
-        .x(function(d) { return d[0] })
-        .y(function(d) { return d[1]/100 }) //adjusting, 100% is 1.00, not 100 as it is in the data
-        .color(d3.scale.category10().range())
-        .useInteractiveGuideline(true)
-        ;
+        var chart = nv.models.lineWithFocusChart();
+
+        var tickMultiFormat = d3.time.format.multi([
+          ["%b %-d", function(d) { return d.getDate() != 1; }], // not the first of the month
+          ["%b %-d", function(d) { return d.getMonth(); }], // not Jan 1st
+          ["%Y", function() { return true; }]
+        ]);
+
         chart.xAxis
-          .tickValues(dataForNV.ticks)
-          // .tickFormat(function(d) {
-          //   return d3.time.format('%x')(new Date(d))
-          // });
+          .tickFormat((d) => tickMultiFormat(new Date(d)));
+
+        chart.x2Axis
+          .tickFormat((d) => tickMultiFormat(new Date(d)));
 
         chart.yAxis
-          .tickFormat(d3.format(',.1%'));
+        .tickFormat(d3.format(',.2'));
+
+        chart.y2Axis
+        .tickFormat(d3.format(',.2'));
 
         d3.select('#chart svg')
           .attr('height', 500)
           .datum(dataForNV.data)
+          .transition().duration(500)
           .call(chart);
 
-        //TODO: Figure out a good way to do this automatically
         nv.utils.windowResize(chart.update);
 
         return chart;
       });
+
     }
   }
 });
 
 Template.dashboard.onRendered(function(){
   const instance = this;
+
+  const opts = {
+    size: 1000,
+    body: {
+      sort: [
+        {
+          date: {
+            order: 'desc',
+          },
+        },
+      ],
+    },
+  };
 
   instance.autorun(() => {
     instance.getChartData({size: 1000})
