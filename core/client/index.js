d80087b8c6034e64a90d73127dfa75f6b8df91cc
Damir Mustafin <frenchthebread@gmail.com>
2017-03-06 15:12:02 +0200

    eslint

--- core/client/index.js
+++ core/client/index.js
@@ -1,4 +1,4 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Template } from 'meteor/templating'
+import { ReactiveVar } from 'meteor/reactive-var'
 
-import './index.html';
+import './index.html'
