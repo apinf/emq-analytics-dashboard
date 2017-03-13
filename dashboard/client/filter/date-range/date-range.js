ef61d8643d9fce8cd2604a6664c03364a245c10f
Damir Mustafin <frenchthebread@gmail.com>
2017-03-13 16:51:39 +0200

    Add pikaday datepicker

--- dashboard/client/filter/date-range/date-range.js
+++ dashboard/client/filter/date-range/date-range.js
@@ -1,28 +1,37 @@
 import { Template } from 'meteor/templating'
 import { FlowRouter } from 'meteor/kadira:flow-router'
 
-import $ from 'jquery'
+import Pikaday from 'pikaday'
 
 Template.filterDateRange.onRendered(() => {
   const from = FlowRouter.getQueryParam('from')
   const to = FlowRouter.getQueryParam('to')
 
+  const dpFrom = new Pikaday({
+    field: $('#datepicker-from')[0],
+    format: 'D MMM YYYY',
+    onSelect () {
+      FlowRouter.setQueryParams({
+        from: this.getMoment().format('YYYY-MM-DD')
+      })
+    }
+  })
+
+  const dpTo = new Pikaday({
+    field: $('#datepicker-to')[0],
+    format: 'D MMM YYYY',
+    onSelect () {
+      FlowRouter.setQueryParams({
+        to: this.getMoment().format('YYYY-MM-DD')
+      })
+    }
+  })
+
   if (from) {
-    $('#analytics-date-from').val(from)
+    dpFrom.setDate(from)
   }
 
   if (to) {
-    $('#analytics-date-to').val(to)
-  }
-})
-
-Template.filterDateRange.events({
-  'change #analytics-date-from': event => {
-    const from = $(event.currentTarget).val()
-    FlowRouter.setQueryParams({ from })
-  },
-  'change #analytics-date-to': event => {
-    const to = $(event.currentTarget).val()
-    FlowRouter.setQueryParams({ to })
+    dpTo.setDate(to)
   }
 })
